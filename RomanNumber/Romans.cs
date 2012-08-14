93ee17ce2352f8f4213dfa1f9d0fb6899c819208
Fatemeh Seifipour <Fa.seifipour@gmail.com>
2012-08-14 01:04:28 +0100

    RomanNumbers

---
+++ RomanNumber/Romans.cs
@@ -0,0 +1,153 @@
+﻿public class RomanNumber
+{
+    protected string GetRomanYekan(int givenNumber)
+    {
+        string romanNumberI = "I";
+        string romanNumberV = "V";
+        string romanNumberX = "X";
+        string romanNumber = null;
+        if (givenNumber == 1)
+        {
+
+            romanNumber = romanNumberI;
+        }
+        if (givenNumber == 2)
+        {
+            romanNumber = string.Concat(romanNumberI, romanNumberI);
+        }
+        if (givenNumber == 3)
+        {
+            romanNumber = string.Concat(romanNumberI, romanNumberI, romanNumberI);
+
+        }
+        if (givenNumber == 4)
+        {
+            romanNumber = string.Concat(romanNumberI, romanNumberV);
+        }
+        if (givenNumber == 5)
+        {
+            romanNumber = string.Concat(romanNumberV);
+        }
+        if (givenNumber == 6)
+        {
+            romanNumber = string.Concat(romanNumberV, romanNumberI);
+        }
+        if (givenNumber == 7)
+        {
+            romanNumber = string.Concat(romanNumberV, romanNumberI, romanNumberI);
+        }
+        if (givenNumber == 8)
+        {
+            romanNumber = string.Concat(romanNumberV, romanNumberI, romanNumberI, romanNumberI);
+        }
+        if (givenNumber == 9)
+        {
+            romanNumber = string.Concat(romanNumberI, romanNumberX);
+        }
+        return romanNumber;
+    }
+
+
+    protected string GetRomanDahgan(int givenNumebr)
+    {
+        string romanNumber = string.Empty;
+        string romanNumberX = "X";
+        string romanNumberL = "L";
+        string romanNumberC = "C";
+        if (givenNumebr == 1)
+        {
+            romanNumber = romanNumberX;
+        }
+        if (givenNumebr == 2)
+        {
+            romanNumber = string.Concat(romanNumberX, romanNumberX);
+        }
+        if (givenNumebr == 3)
+        {
+            romanNumber = string.Concat(romanNumberX, romanNumberX, romanNumberX);
+        }
+        if (givenNumebr == 4)
+        {
+            romanNumber = string.Concat(romanNumberX, romanNumberL);
+        }
+        if (givenNumebr == 5)
+        {
+            romanNumber = romanNumberL;
+        }
+        if (givenNumebr == 6)
+        {
+            romanNumber = string.Concat(romanNumberL, romanNumberX);
+        }
+        if (givenNumebr == 7)
+        {
+            romanNumber = string.Concat(romanNumberL, romanNumberX, romanNumberX);
+        }
+        if (givenNumebr == 8)
+        {
+            romanNumber = string.Concat(romanNumberL, romanNumberX, romanNumberX, romanNumberX);
+        }
+        if (givenNumebr == 9)
+        {
+            romanNumber = string.Concat(romanNumberX, romanNumberC);
+        }
+        return romanNumber;
+    }
+
+    protected string GetRomanSadgan(int givenNumebr)
+    {
+        string romanNumber = string.Empty;
+        string romanNumberD = "D";
+        string romanNumberM = "M";
+        string romanNumberC = "C";
+        if (givenNumebr == 1)
+        {
+            romanNumber = romanNumberC;
+        }
+        if (givenNumebr == 2)
+        {
+            romanNumber = string.Concat(romanNumberC, romanNumberC);
+        }
+        if (givenNumebr == 3)
+        {
+            romanNumber = string.Concat(romanNumberC, romanNumberC, romanNumberC);
+        }
+        if (givenNumebr == 4)
+        {
+            romanNumber = string.Concat(romanNumberC, romanNumberD);
+        }
+        if (givenNumebr == 5)
+        {
+            romanNumber = romanNumberD;
+        }
+        if (givenNumebr == 6)
+        {
+            romanNumber = string.Concat(romanNumberD, romanNumberC);
+        }
+        if (givenNumebr == 7)
+        {
+            romanNumber = string.Concat(romanNumberD, romanNumberC, romanNumberC);
+        }
+        if (givenNumebr == 8)
+        {
+            romanNumber = string.Concat(romanNumberD, romanNumberC, romanNumberC, romanNumberC);
+        }
+        if (givenNumebr == 9)
+        {
+            romanNumber = string.Concat(romanNumberC, romanNumberM);
+        }
+        return romanNumber;
+    }
+
+
+    public string GetRomanNumber(int givenNumebr)
+    {
+        string romanNumber = string.Empty;
+        int sadgan = givenNumebr / 100;
+        int sad = givenNumebr % 100;
+        int dahgan = (sad / 10);
+        int yekan = givenNumebr % 10;
+
+        romanNumber = string.Concat(GetRomanSadgan(sadgan), GetRomanDahgan(dahgan), GetRomanYekan(yekan));
+        return romanNumber;
+    }
+}
---
+++ RomanNumber/TestRomanNumbers.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+
+namespace SuperMarket
+{
+    [TestFixture]
+    public class TestRomanNumbers:RomanNumber
+    {
+
+        [TestCase(1,"I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        public void Given_Yekan_when_GetRomanNumber_Then_Return_RomanNumber(int giveNumber, string expectedRomanNumber)
+        {
+
+
+            var number =GetRomanYekan(giveNumber);
+            Assert.That(number, Is.EqualTo(expectedRomanNumber));
+        }
+
+        [TestCase(1, "X")]
+        [TestCase(2, "XX")]
+        [TestCase(3, "XXX")]
+        [TestCase(4, "XL")]
+        [TestCase(5, "L")]
+        [TestCase(6, "LX")]
+        [TestCase(7, "LXX")]
+        [TestCase(8, "LXXX")]
+        [TestCase(9, "XC")]
+        [Test]
+        public void Given_Dahgan_when_GetRomanNumber_Then_Return_RomanNumber(int givennumber, string expectedRomanNumber)
+        {
+            string number =GetRomanDahgan(givennumber);
+            Assert.That(number, Is.EqualTo(expectedRomanNumber));
+        }
+
+        [TestCase(1, "C")]
+        [TestCase(2, "CC")]
+        [TestCase(3, "CCC")]
+        [TestCase(4, "CD")]
+        [TestCase(5, "D")]
+        [TestCase(6, "DC")]
+        [TestCase(7, "DCC")]
+        [TestCase(8, "DCCC")]
+        [TestCase(9, "CM")]
+        [Test]
+        public void Given_sadgan_when_GetRomanNumber_Then_Return_RomanNumber(int givennumber, string expectedRomanNumber)
+        {
+            string number = GetRomanSadgan(givennumber);
+            Assert.That(number, Is.EqualTo(expectedRomanNumber));
+        }
+
+
+
+
+        [TestCase(11, "XI")]
+        [TestCase(24, "XXIV")]
+        [TestCase(37, "XXXVII")]
+        [TestCase(43, "XLIII")]
+        [TestCase(50, "L")]
+        [TestCase(63, "LXIII")]
+        [TestCase(72, "LXXII")]
+        [TestCase(88, "LXXXVIII")]
+        [TestCase(99, "XCIX")]
+        [TestCase(311, "CCCXI")]
+        [TestCase(224, "CCXXIV")]
+        [TestCase(537, "DXXXVII")]
+        [TestCase(743, "DCCXLIII")]
+        [TestCase(150, "CL")]
+        [TestCase(863, "DCCCLXIII")]
+        [TestCase(972, "CMLXXII")]
+        [TestCase(88, "LXXXVIII")]
+        [TestCase(299, "CCXCIX")]
+        [Test]
+        public void Given_Number_when_GetRomanNumber_Return_RomanNumebr(int givenNumebr, string expectedNumber)
+        {
+           RomanNumber romanNumber=new RomanNumber();   
+            string roman = romanNumber.GetRomanNumber(givenNumebr);
+            Assert.That(roman, Is.EqualTo(expectedNumber));
+        }
+
+
+    }
+
+}
